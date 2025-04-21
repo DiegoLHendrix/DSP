63d8b40e33a535ce1cba658db359a74b3c5cbc5e
DiegoLHendrix <diegolhendrix@gmail.com>
2025-04-21 08:48:59 -0400

    Formatting changes and block diagram

--- lab10 Breathing Rate Monitor/include/fir.hpp
+++ lab10 Breathing Rate Monitor/include/fir.hpp
@@ -1,55 +1,55 @@
 namespace fir {
 
 int FIR_Generic(long inputX, int sampleNumber) {
-  // Starting with a generic FIR filter impelementation customize only by
-  // changing the length of the filter using MFILT and the values of the
-  // impulse response in h
+    // Starting with a generic FIR filter impelementation customize only by
+    // changing the length of the filter using MFILT and the values of the
+    // impulse response in h
 
-  // Filter type: FIR
+    // Filter type: FIR
 
-  //
-  //  Set the constant HFXPT to the sum of the values of the impulse response
-  //  This is to keep the gain of the impulse response at 1.
-  //
-  const int HFXPT = 1, MFILT = 4;
+    //
+    //  Set the constant HFXPT to the sum of the values of the impulse response
+    //  This is to keep the gain of the impulse response at 1.
+    //
+    const int HFXPT = 1, MFILT = 4;
 
-  int h[] = {};
+    int h[] = {};
 
-  int i;
-  const float INV_HFXPT = 1.0 / HFXPT;
-  static long xN[MFILT] = {inputX};
-  long yOutput = 0;
+    int i;
+    const float INV_HFXPT = 1.0 / HFXPT;
+    static long xN[MFILT] = {inputX};
+    long yOutput = 0;
 
-  //
-  // Right shift old xN values. Assign new inputX to xN[0];
-  //
-  for (i = (MFILT - 1); i > 0; i--) {
-    xN[i] = xN[i - 1];
-  }
-  xN[0] = inputX;
+    //
+    // Right shift old xN values. Assign new inputX to xN[0];
+    //
+    for (i = (MFILT - 1); i > 0; i--) {
+        xN[i] = xN[i - 1];
+    }
+    xN[0] = inputX;
 
-  //
-  // Convolve the input sequence with the impulse response
-  //
+    //
+    // Convolve the input sequence with the impulse response
+    //
 
-  for (i = 0; i < MFILT; i++) {
+    for (i = 0; i < MFILT; i++) {
 
-    // Explicitly cast the impulse value and the input value to LONGs then
-    // multiply by the input value.  Sum up the output values
+        // Explicitly cast the impulse value and the input value to LONGs then
+        // multiply by the input value.  Sum up the output values
 
-    yOutput = yOutput + long(h[i]) * long(xN[i]);
-  }
+        yOutput = yOutput + long(h[i]) * long(xN[i]);
+    }
 
-  //  Return the output, but scale by 1/HFXPT to keep the gain to 1
-  //  Then cast back to an integer
-  //
+    //  Return the output, but scale by 1/HFXPT to keep the gain to 1
+    //  Then cast back to an integer
+    //
 
-  // Skip the first MFILT  samples to avoid the transient at the beginning due
-  // to end effects
-  if (sampleNumber < MFILT) {
-    return long(0);
-  } else {
-    return long(float(yOutput) * INV_HFXPT);
-  }
+    // Skip the first MFILT  samples to avoid the transient at the beginning due
+    // to end effects
+    if (sampleNumber < MFILT) {
+        return long(0);
+    } else {
+        return long(float(yOutput) * INV_HFXPT);
+    }
 }
-} // namespace fir
+}  // namespace fir
--- lab10 Breathing Rate Monitor/src/main.cpp
+++ lab10 Breathing Rate Monitor/src/main.cpp
@@ -18,153 +18,144 @@
 //**********************************************************************
 void setup() {
 
-  core::configureArduino();
-  Serial.begin(115200);
-  delay(5);
+    core::configureArduino();
+    Serial.begin(115200);
+    delay(5);
 
-  // Handshake with MATLAB
-  Serial.println(F("%Arduino Ready"));
-  while (Serial.read() != 'g')
-    ; // spin
+    // Handshake with MATLAB
+    Serial.println(F("%Arduino Ready"));
+    while (Serial.read() != 'g')
+        ;  // spin
 
-  MsTimer2::set(core::TSAMP_MSEC,
-                core::ISR_Sample); // Set sample msec, ISR name
-  MsTimer2::start();               // start running the Timer
+    MsTimer2::set(core::TSAMP_MSEC, core::ISR_Sample);  // Set sample msec, ISR name
+    MsTimer2::start();                                  // start running the Timer
 }
 
 ////**********************************************************************
 void loop() {
 
-  // syncSample();  // Wait for the interupt when actually reading ADC data
+    // syncSample();  // Wait for the interupt when actually reading ADC data
 
-  // Breathing Rate Detection
+    // Breathing Rate Detection
 
-  // Declare variables
+    // Declare variables
 
-  float readValue,
-      floatOutput; //  Input data from ADC after dither averaging or from MATLAB
-  long fxdInputValue, lpfInput, lpfOutput;
-  long eqOutput; //  Equalizer output
-  int alarmCode; //  Alarm code
+    float readValue, floatOutput;  //  Input data from ADC after dither averaging or from MATLAB
+    long fxdInputValue, lpfInput, lpfOutput;
+    long eqOutput;  //  Equalizer output
+    int alarmCode;  //  Alarm code
 
-  // ******************************************************************
-  //  When finding the impulse responses of the filters use this as an input
-  //  Create a Delta function in time with the first sample a 1 and all others 0
-  core::xv = (core::loopTick == 0) ? 1.0 : 0.0; // impulse test input
+    // ******************************************************************
+    //  When finding the impulse responses of the filters use this as an input
+    //  Create a Delta function in time with the first sample a 1 and all others 0
+    core::xv = (core::loopTick == 0) ? 1.0 : 0.0;  // impulse test input
 
-  // ******************************************************************
-  //  Use this when the test vector generator is used as an input
-  //  xv = testVector();
+    // ******************************************************************
+    //  Use this when the test vector generator is used as an input
+    //  xv = testVector();
 
-  // ******************************************************************
-  //  Read input value in ADC counts  -- Get simulated data from MATLAB
-  // readValue = ReadFromMATLAB();
+    // ******************************************************************
+    //  Read input value in ADC counts  -- Get simulated data from MATLAB
+    // readValue = ReadFromMATLAB();
 
-  // ******************************************************************
-  //  Read input value from ADC using Dithering, and averaging
-  readValue = core::analogReadDitherAve();
+    // ******************************************************************
+    //  Read input value from ADC using Dithering, and averaging
+    readValue = core::analogReadDitherAve();
 
-  //  Convert the floating point number to a fixed point value.  First
-  //  scale the floating point value by a number to increase its resolution
-  //  (use DATA_FXPT).  Then round the value and truncate to a fixed point
-  //  INT datatype
+    //  Convert the floating point number to a fixed point value.  First
+    //  scale the floating point value by a number to increase its resolution
+    //  (use DATA_FXPT).  Then round the value and truncate to a fixed point
+    //  INT datatype
 
-  // fxdInputValue = long(DATA_FXPT * readValue + 0.5);
+    // fxdInputValue = long(DATA_FXPT * readValue + 0.5);
 
-  //  Execute the equalizer
-  //  eqOutput = EqualizerFIR( fxdInputValue, loopTick );
+    //  Execute the equalizer
+    //  eqOutput = EqualizerFIR( fxdInputValue, loopTick );
 
-  //  Execute the noise filter.
-  // eqOutput = NoiseFilter( eqOutput, loopTick );
+    //  Execute the noise filter.
+    // eqOutput = NoiseFilter( eqOutput, loopTick );
 
-  //  Convert the output of the equalizer by scaling floating point
-  // xv = float(eqOutput) * INV_FXPT;
+    //  Convert the output of the equalizer by scaling floating point
+    // xv = float(eqOutput) * INV_FXPT;
 
-  //*******************************************************************
-  // Uncomment this when measuring execution times
-  core::startUsec = micros();
+    //*******************************************************************
+    // Uncomment this when measuring execution times
+    core::startUsec = micros();
 
-  // ******************************************************************
-  //  Compute the output of the filter using the cascaded SOS sections
-  core::yv = iir::IIR_LPF(core::xv); // second order systems cascade
+    // ******************************************************************
+    //  Compute the output of the filter using the cascaded SOS sections
+    core::yv = iir::IIR_LPF(core::xv);  // second order systems cascade
 
-  //  Compute the output of the filter using the cascaded SOS sections
-  core::yLF = iir::IIR_LPF(core::xv); // second order systems cascade
-  core::yMF = iir::IIR_BPF(core::xv);
-  core::yHF = iir::IIR_HPF(core::xv);
+    //  Compute the output of the filter using the cascaded SOS sections
+    core::yLF = iir::IIR_LPF(core::xv);  // second order systems cascade
+    core::yMF = iir::IIR_BPF(core::xv);
+    core::yHF = iir::IIR_HPF(core::xv);
 
-  //  Compute the latest output of the running stats for the output of the
-  //  filters. Pass the entire set of output values, the latest stats structure
-  //  and the reset flag
-  core::statsReset = ((core::statsLF.tick % 100) == 0);
-  core::getStats(core::yLF, core::statsLF, core::statsReset);
-  core::stdLF = core::statsLF.stdev;
+    //  Compute the latest output of the running stats for the output of the
+    //  filters. Pass the entire set of output values, the latest stats structure
+    //  and the reset flag
+    core::statsReset = ((core::statsLF.tick % 100) == 0);
+    core::getStats(core::yLF, core::statsLF, core::statsReset);
+    core::stdLF = core::statsLF.stdev;
 
-  core::statsReset = ((core::statsMF.tick % 100) == 0);
-  core::getStats(core::yMF, core::statsMF, core::statsReset);
-  core::stdMF = core::statsMF.stdev;
+    core::statsReset = ((core::statsMF.tick % 100) == 0);
+    core::getStats(core::yMF, core::statsMF, core::statsReset);
+    core::stdMF = core::statsMF.stdev;
 
-  core::statsReset = ((core::statsHF.tick % 100) == 0);
-  core::getStats(core::yHF, core::statsHF, core::statsReset);
-  core::stdHF = core::statsHF.stdev;
+    core::statsReset = ((core::statsHF.tick % 100) == 0);
+    core::getStats(core::yHF, core::statsHF, core::statsReset);
+    core::stdHF = core::statsHF.stdev;
 
-  //  Compute the latest output of the running stats for the output of the
-  //  filters. Pass the entire set of output values, the latest stats structure
-  //  and the reset flag
+    //  Compute the latest output of the running stats for the output of the
+    //  filters. Pass the entire set of output values, the latest stats structure
+    //  and the reset flag
 
-  //  statsReset = (statsLF.tick%100 == 0);
-  //  getStats( yv, statsLF, statsReset);
-  //  stdLF = statsLF.stdev;
+    //  statsReset = (statsLF.tick%100 == 0);
+    //  getStats( yv, statsLF, statsReset);
+    //  stdLF = statsLF.stdev;
 
-  //*******************************************************************
-  // Uncomment this when measuring execution times
-  // endUsec = micros();
-  // execUsec = execUsec + (endUsec-startUsec);
+    //*******************************************************************
+    // Uncomment this when measuring execution times
+    // endUsec = micros();
+    // execUsec = execUsec + (endUsec-startUsec);
 
-  //  Call the alarm check function to determine what breathing range
-  //  alarmCode = AlarmCheck( stdLF, stdMF, stdHF );
+    //  Call the alarm check function to determine what breathing range
+    //  alarmCode = AlarmCheck( stdLF, stdMF, stdHF );
 
-  //  Call the alarm function to turn on or off the tone
-  // setAlarm(alarmCode, isToneEn );
+    //  Call the alarm function to turn on or off the tone
+    // setAlarm(alarmCode, isToneEn );
 
-  // To print data to the serial port, use the WriteToSerial function.
-  //
-  //  This is a generic way to print out variable number of values
-  //
-  //  There are two input arguments to the function:
-  //  printArray -- An array of values that are to be printed starting with the
-  //  first column numValues -- An integer indicating the number of values in
-  //  the array.
+    // To print data to the serial port, use the WriteToSerial function.
+    //
+    //  This is a generic way to print out variable number of values
+    //
+    //  There are two input arguments to the function:
+    //  printArray -- An array of values that are to be printed starting with the
+    //  first column numValues -- An integer indicating the number of values in
+    //  the array.
 
-  core::printArray[0] =
-      core::loopTick;             //  The sample number -- always print this
-  core::printArray[1] = core::xv; //  Column 2
+    core::printArray[0] = core::loopTick;  //  The sample number -- always print this
+    core::printArray[1] = core::xv;        //  Column 2
 
-  //   printArray[2] = yLF;       //  Column 3
-  //   printArray[3] = yMF;       //  Column 4, etc...
-  //   printArray[4] = yHF;
-  //   printArray[5] = stdLF;
-  //   printArray[6] = stdMF;
-  //   printArray[7] = stdHF;
-  //   printArray[8] = float(alarmCode);
+    //   core::printArray[2] = core::yLF;       //  Column 3
+    //   core::printArray[3] = core::yMF;       //  Column 4, etc...
+    //   core::printArray[4] = core::yHF;
+    //   core::printArray[5] = core::stdLF;
+    //   core::printArray[6] = core::stdMF;
+    //   core::printArray[7] = core::stdHF;
+    //   core::printArray[8] = core::float(alarmCode);
 
-  core::numValues =
-      2; // The number of columns to be sent to the serial monitor (or MATLAB)
+    core::numValues = 2;  // The number of columns to be sent to the serial monitor (or MATLAB)
 
-  core::WriteToSerial(
-      core::numValues,
-      core::printArray); //  Write to the serial monitor (or MATLAB)
+    core::WriteToSerial(core::numValues, core::printArray);  //  Write to the serial monitor (or MATLAB)
 
-  if (core::loopTick++ >= core::NUM_SAMPLES) {
-    // Serial.print("Average execution time (uSec) = ");Serial.println(
-    // float(execUsec)/NUM_SAMPLES );
-    while (true)
-      ; // spin forever
-  }
+    if (core::loopTick++ >= core::NUM_SAMPLES) {
+        // Serial.print("Average execution time (uSec) = ");Serial.println(
+        // float(execUsec)/NUM_SAMPLES );
+        while (true)
+            ;  // spin forever
+    }
 
-} // loop()
+}  // loop()
 
 ////**********************************************************************
-
-// put function definitions here:
-int myFunction(int x, int y) { return x + y; }
